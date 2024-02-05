23e053a7bcb92b3a7e80709ebc4036d27b3c4bfb
landryarki <landry.gigant@epitech.eu>
2024-02-05 16:10:30 +0100

    :construction: AComponent.hpp

--- AComponent.hpp
+++ AComponent.hpp
@@ -17,8 +17,7 @@ namespace nts {
         AComponent() = default;
         AComponent(std::size_t nbPins);
         ~AComponent() = default;
-        virtual void simulate(std::size_t tick) override;
-        virtual nts::Tristate compute(std::size_t pin) override;
+        virtual void simulate(std::size_t tick) override {};
         virtual void setLink(std::size_t pin, nts::IComponent& other, std::size_t otherPin) override;
         virtual nts::Tristate getLink(std::size_t pin) const override;
         virtual std::size_t getNbPins() const override;
