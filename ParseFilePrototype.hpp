3b1d4f3ed8fa144623e36b9e4b37197f6b4e15a2
PerAlban <alban.peralta@epitech.eu>
2024-02-15 19:11:26 +0100

    Revert ":fire: Delete useless file"
    This reverts commit 52b7763d0d73e0c9f430329fbc35076014e89ef4.

---
+++ ParseFilePrototype.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** ParseFilePrototype.hpp
+** File description:
+** ParseFilePrototype.hpp
+*/
+
+#pragma once
+
+/**
+ * @brief Parses the command line arguments.
+ *
+ * This function is responsible for parsing the command line arguments
+ * provided to the program. It is expected to handle the arguments and
+ * perform necessary actions based on them.
+ *
+ * @param ac The count of command line arguments.
+ * @param av The array of command line arguments.
+ */
+void parseFile(int ac, char **av);
