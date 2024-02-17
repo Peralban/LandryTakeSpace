66e13c6a7f16c97e505627c50a41757316e1f520
PerAlban <alban.peralta@epitech.eu>
2024-02-17 22:15:46 +0100

    :sparkles: Create Main loop of the nts

---
+++ NanotekSpice.cpp
@@ -0,0 +1,115 @@
+/*
+** EPITECH PROJECT, 2024
+** NanotekSpice.cpp
+** File description:
+** Created by Alban PERALTA et Landry GIGANT
+*/
+
+#include "NanotekSpice.hpp"
+
+static void checkExistence(nts::allInputAndNameInVector Inputs, std::string src)
+{
+    std::string name = src.substr(0, src.find("="));
+    std::string pin = src.substr(src.find("=") + 1);
+    size_t i = 0;
+
+    for (; i < Inputs.size(); i++)
+        if (name == Inputs[i].second)
+            break;
+    if (i == Inputs.size())
+        throw nts::Error("Error: '" + name + "' does not exist");
+    if (pin != "1" && pin != "0" && pin != "U")
+        throw nts::Error("Error: invalid value");
+    if (Inputs[i].first->isUnused(2)) {
+        if (Inputs[i].first->getState(1) == nts::Tristate::True)
+            throw nts::Error("Error: '" + name + "' is set to True and cannot be changed");
+        if (Inputs[i].first->getState(1) == nts::Tristate::False)
+            throw nts::Error("Error: '" + name + "' is set to False and cannot be changed");
+    }
+}
+
+static std::string getValue(nts::Tristate value)
+{
+    if (value == nts::Tristate::True)
+        return "1";
+    if (value == nts::Tristate::False)
+        return "0";
+    return "U";
+}
+
+static void display(nts::allInputAndNameInVector Inputs, nts::allOutputAndNameInVector Outputs, size_t tick)
+{
+    std::cout << "tick: " << tick << std::endl;
+    std::cout << "input(s):" << std::endl;
+    for (auto &input : Inputs)
+        std::cout << input.second << ": " << getValue(input.first->compute(1)) << std::endl;
+    std::cout << "output(s):" << std::endl;
+    for (auto &output : Outputs)
+        std::cout << output.second << ": " << getValue(output.first->compute(1)) << std::endl;
+}
+
+static void simulate(nts::allInputAndNameInVector Inputs, nts::allOutputAndNameInVector Outputs, size_t &tick,
+                     std::vector<std::pair<std::string, std::string>> &saveValue)
+{
+    for (auto &input : Inputs) {
+        for (auto &value : saveValue) {
+            if (input.second == value.first) {
+                if (value.second == "1")
+                    if (!input.first->isUnused(5))
+                        input.first->compute(3);
+                    else
+                        input.first->compute(4);
+                else if (value.second == "0")
+                    if (!input.first->isUnused(5))
+                        input.first->compute(4);
+                    else
+                        input.first->compute(3);
+                else
+                    input.first->compute(2);
+            }
+        }
+    }
+    tick++;
+    for (auto &input : Inputs)
+        if (!input.first->isUnused(5))
+            input.first->compute(5);
+    for (auto &output : Outputs)
+        output.first->compute(1);
+    saveValue.clear();
+}
+
+void nts::NanotekSpice::execShell()
+{
+    std::string line;
+    std::cout << "❯ ";
+    while (getline(std::cin, line)) {
+        if (line.empty())
+            continue;
+        if (line.find("=") != std::string::npos) {
+            try {
+                checkExistence(_inputs, line);
+                _saveValue.push_back(std::make_pair(line.substr(0, line.find("=")),
+                                                    line.substr(line.find("=") + 1)));
+                std::cout << "❯ ";
+                continue;
+            } catch (nts::Error &e) {
+                std::cerr << e.what() << std::endl;
+                std::cout << "❯ ";
+                continue;
+            }
+        } if (line == "display") {
+            display(_inputs, _outputs, _tick);
+        } else if (line == "simulate") {
+            try {
+                simulate(_inputs, _outputs, _tick, _saveValue);
+            } catch (nts::Error &e) {
+                std::cerr << e.what() << std::endl;
+            }
+        } else if (line == "exit") {
+            exit(0);
+        } else {
+            std::cerr << "Error: invalid command" << std::endl;
+        }
+        std::cout << "❯ ";
+    }
+}
---
+++ NanotekSpice.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** NanotekSpice.hpp
+** File description:
+** Created by Alban PERALTA et Landry GIGANT
+*/
+
+#pragma once
+
+#include "ParseFile.hpp"
+
+namespace nts {
+    class NanotekSpice {
+    public:
+        NanotekSpice(nts::allInputAndNameInVector Inputs, nts::allOutputAndNameInVector Outputs) :
+            _inputs(Inputs), _outputs(Outputs), _tick(0) {}
+        ~NanotekSpice() = default;
+        void execShell();
+    private:
+        std::vector<std::pair<std::string, std::string>> _saveValue;
+        nts::allInputAndNameInVector _inputs;
+        nts::allOutputAndNameInVector _outputs;
+        size_t _tick;
+    };
+}
