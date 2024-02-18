43230b43a7a0c6bbd7d44bde5637c65105e57cd2
PerAlban <alban.peralta@epitech.eu>
2024-02-18 03:41:43 +0100

    ✅ Create Tests for Parsing

---
+++ Test/Test_include.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** LandryTakeSpace
+** File description:
+** No file there , just an epitech header example .
+*/
+
+#pragma once
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+void redirect_stdin(void);
